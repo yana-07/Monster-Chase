a868182080dc47fec8fb13a2c78c6fb443af7bc6
yana.radeva7 <yana.radeva7@gmail.com>
2023-01-13 13:01:25 +0200

    create a coroutine that spawns random enemies from the left/right side of the scene setting a random speed

---
+++ Monster Chase/Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] monsterReference;
+
+    private GameObject spawnedMonster;
+
+    [SerializeField]
+    private Transform leftPos, rightPos;
+
+    private int randomIndex;
+    private int randomSide;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine(SpawnMonsters());
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnMonsters()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1, 5));
+
+            randomIndex = Random.Range(0, monsterReference.Length);
+            randomSide = Random.Range(0, 2);
+
+            // creates a copy of the passed game object reference
+            spawnedMonster = Instantiate(monsterReference[randomIndex]);
+
+            if (randomSide == 0)
+            {
+                // left side
+                spawnedMonster.transform.position = leftPos.position;
+                spawnedMonster.GetComponent<Monster>().speed = Random.Range(4, 10);
+            }
+            else
+            {
+                // right side
+                spawnedMonster.transform.position = rightPos.position;
+                spawnedMonster.GetComponent<Monster>().speed = -Random.Range(4, 10);
+                spawnedMonster.transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+        }        
+    }
+}
